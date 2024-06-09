65e038e0b1a8602676f58ac706f498e4663f9e43
Camilo Reyes <reyesc@comcast.net>
2024-06-09 14:11:05 -0500

    Initial commit

---
+++ src/NetAwsAsyncprocessor/NetAwsAsyncprocessorStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace NetAwsAsyncprocessor
+{
+    public class NetAwsAsyncprocessorStack : Stack
+    {
+        internal NetAwsAsyncprocessorStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
